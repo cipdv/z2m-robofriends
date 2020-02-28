e5955329e587fd53fd2dfaf2d7f6e26f03bb6f9e
cipdv <cip.devries@gmail.com>
2020-02-28 12:13:07 -0500

    first commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Robot 1',
+    username: 'Clink',
+    email: 'dsdkfjsal@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Robot 2',
+    username: 'Clunk',
+    email: 'werwrwerw@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Robot 3',
+    username: 'Crank',
+    email: 'sdfsfsdfs@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Robot 4',
+    username: 'Bob',
+    email: 'sdfdfs@gmail.com'
+  }
+]
